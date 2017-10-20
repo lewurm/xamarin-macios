07bcf54761b9c74ee7d5404ccfdf5b649a287f34
Rodrigo Kumpera <kumpera@gmail.com>
2017-10-20 11:29:45 -0700

    [interpreter] Fix the DISABLE_REMOTING build.

--- mono/mini/interp/interp.c
+++ mono/mini/interp/interp.c
@@ -257,6 +257,7 @@ lookup_imethod (MonoDomain *domain, MonoMethod *method)
 	return rtm;
 }
 
+#ifndef DISABLE_REMOTING
 gpointer
 mono_interp_get_remoting_invoke (gpointer imethod, MonoError *error)
 {
@@ -266,6 +267,7 @@ mono_interp_get_remoting_invoke (gpointer imethod, MonoError *error)
 
 	return mono_interp_get_imethod (mono_domain_get (), mono_marshal_get_remoting_invoke (imethod_cast->method), error);
 }
+#endif
 
 InterpMethod*
 mono_interp_get_imethod (MonoDomain *domain, MonoMethod *method, MonoError *error)
